5da6515bc0c68cb094239d2f01f6f3d4eff8d264
maxime123456789517 <mbreuil7@gmail.com>
2025-03-31 11:55:40 +0200

    Update Graph.cs

--- Graph.cs
+++ Graph.cs
@@ -1,133 +1,271 @@
-﻿using Projet_PSI;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using.IO;
+using Projet_PSI;
 
-namespace Projet_PSI
+namespace projet_algo_2
 {
     internal class Graph
     {
+        /// <summary>
+        /// Dictionnaire contenant les nœuds du graphe.
+        /// </summary>
         private Dictionary<int, Noeud> noeuds;
+
+        /// <summary>
+        /// Liste d'adjacence représentant les connexions entre les nœuds.
+        /// </summary>
         private Dictionary<int, List<int>> listeAdjacence;
+
+        /// <summary>
+        /// Liste des liens entre les nœuds.
+        /// </summary>
+        private List<Lien> liens;
+
+        /// <summary>
+        /// Matrice d'adjacence représentant le graphe sous forme matricielle.
+        /// </summary>
         private int[,] matriceAdjacence;
+
+        /// <summary>
+        /// Nombre total de nœuds dans le graphe.
+        /// </summary>
         private int nombreNoeuds = 34;
+
+        /// <summary>
+        /// Nombre total de liens dans le graphe.
+        /// </summary>
         private int nombreLiens = 78;
 
-        public Dictionary<int, List<int>> ListeAdjacence
-        {
-            get { return this.listeAdjacence; }
+        public Dictionary<int, List<int>> ListeAdjacence => listeAdjacence;
+        public int[,] MatriceAdjacence => matriceAdjacence;
+        public Dictionary<int, Noeud> Noeuds => noeuds;
 
-        }
-        public int[,] MatrciceAdjacence
-        {
-            get { return this.matriceAdjacence; }
-        }
-        public Dictionary<int, Noeud> Noeuds
-        {
-            get { return this.noeuds; }
-        }
-
-
+        /// <summary>
+        /// Constructeur de la classe Graph. Initialise les structures de données.
+        /// </summary>
         public Graph()
         {
-            int id = 4;
+            noeuds = new Dictionary<int, Noeud>();
+            listeAdjacence = new Dictionary<int, List<int>>();
+            liens = new List<Lien>();
+            matriceAdjacence = new int[nombreNoeuds + 1, nombreNoeuds + 1];
 
-
-            Noeud noeud1 = new Noeud(id);
-            Noeud noeud2 = new Noeud(id);
-            Lien lien1 = new Lien(noeud1, noeud2);
-            Dictionary<int, Noeud> noeuds = new Dictionary<int, Noeud>();
-            Dictionary<Noeud, List<Noeud>> listeAdjacence = new Dictionary<Noeud, List<Noeud>>();
-            int[,] matrciceAdjacence = new int[nombreNoeuds + 1, nombreNoeuds + 1];
-            List<Lien> liens = new List<Lien>();
+            for (int i = 1; i <= nombreNoeuds; i++)
+            {
+                noeuds.Add(i, new Noeud(i));
+                listeAdjacence.Add(i, new List<int>());
+            }
         }
-        public void readLine(string fichier)
-        {
 
+        /// <summary>
+        /// Lit un fichier contenant la structure du graphe et construit le graphe.
+        /// </summary>
+        /// <param name="fichier">Chemin du fichier à lire.</param>
+        public void LireFichier(string fichier)
+        {
             try
             {
-                StreamReader flux = new StreamReader(fichier);
-
-
-                for (int i = 0; i < 23; i++)
+                using (StreamReader flux = new StreamReader(fichier))
                 {
-                    flux.ReadLine();
-                }
+                    for (int i = 0; i < 23; i++)
+                        flux.ReadLine();
 
-
-                for (int i = 0; i < 78; i++)
-                {
-                    string ligne = flux.ReadLine();
-                    if (string.IsNullOrWhiteSpace(ligne))
+                    for (int i = 0; i < nombreLiens; i++)
                     {
-                        continue;
+                        string ligne = flux.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(ligne))
+                        {
+                            string[] valeurs = ligne.Split(' ');
+                            int noeud1 = int.Parse(valeurs[0]);
+                            int noeud2 = int.Parse(valeurs[1]);
+
+                            AjouterLien(noeud1, noeud2);
+                        }
                     }
-                    string[] valeurs = ligne.Split(' ');
-                    int noeud1 = int.Parse(valeurs[0]);
-                    int noeud2 = int.Parse(valeurs[1]);
-
-                    AjouterLien(noeud1, noeud2);
                 }
-                flux.Close();
-
-
-
-
             }
             catch (Exception e)
             {
                 Console.WriteLine("Erreur lors de la lecture du fichier : " + e.Message);
             }
-
         }
 
+        /// <summary>
+        /// Ajoute un lien entre deux nœuds.
+        /// </summary>
         public void AjouterLien(int n1, int n2)
         {
-            if (!listeAdjacence.ContainsKey(n1))
+            if (!listeAdjacence.ContainsKey(n1)) listeAdjacence[n1] = new List<int>();
+            if (!listeAdjacence.ContainsKey(n2)) listeAdjacence[n2] = new List<int>();
+
+            if (!listeAdjacence[n1].Contains(n2))
             {
-                listeAdjacence[n1] = new List<int>();
+                listeAdjacence[n1].Add(n2);
+                listeAdjacence[n2].Add(n1);
+                liens.Add(new Lien(noeuds[n1], noeuds[n2]));
+
+                matriceAdjacence[n1, n2] = 1;
+                matriceAdjacence[n2, n1] = 1;
             }
-
-            if (!listeAdjacence.ContainsKey(n2))
-            {
-                listeAdjacence[n2] = new List<int>();
-            }
-
-            listeAdjacence[n1].Add(n2);
-            listeAdjacence[n2].Add(n1);
-
-
         }
-        public void ConstruireMatriceAdjacence()
+
+        /// <summary>
+        /// Affiche la liste d'adjacence du graphe.
+        /// </summary>
+        public void AfficherListeAdjacence()
         {
-            for (int i = 0; i <= nombreNoeuds; i++)
+            Console.WriteLine("Liste d'adjacence :");
+            foreach (var kvp in listeAdjacence)
             {
-                for (int j = 0; j <= nombreNoeuds; j++)
-                {
-                    matriceAdjacence[i, j] = 0;
-                }
+                Console.Write(kvp.Key + " -> ");
+                Console.WriteLine(string.Join(", ", kvp.Value));
+            }
+        }
+
+        /// <summary>
+        /// Affiche la matrice d'adjacence du graphe.
+        /// </summary>
+        public void AfficherMatriceAdjacence()
+        {
+            Console.WriteLine("Matrice d'adjacence :");
+            for (int i = 1; i <= nombreNoeuds; i++)
+            {
+                for (int j = 1; j <= nombreNoeuds; j++)
+                    Console.Write(matriceAdjacence[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Effectue un parcours en largeur du graphe (BFS).
+        /// </summary>
+        public void ParcoursLargeur(int depart)
+        {
+            if (!noeuds.ContainsKey(depart))
+            {
+                Console.WriteLine("Le nœud de départ n'existe pas !");
+                return;
             }
 
+            Queue<int> file = new Queue<int>();
+            HashSet<int> visite = new HashSet<int>();
 
-            foreach (var noeud in listeAdjacence)
+            file.Enqueue(depart);
+            visite.Add(depart);
+
+            Console.WriteLine("Parcours en Largeur (BFS) :");
+            while (file.Count > 0)
             {
-                int n1 = noeud.Key;
-                foreach (int n2 in noeud.Value)
+                int noeud = file.Dequeue();
+                Console.Write(noeud + " ");
+
+                foreach (int voisin in listeAdjacence[noeud])
                 {
-                    matriceAdjacence[n1, n2] = 1;
-                    matriceAdjacence[n2, n1] = 1;
+                    if (!visite.Contains(voisin))
+                    {
+                        file.Enqueue(voisin);
+                        visite.Add(voisin);
+                    }
                 }
             }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Effectue un parcours en profondeur du graphe (DFS).
+        /// </summary>
+        public void ParcoursProfondeur(int depart)
+        {
+            if (!noeuds.ContainsKey(depart))
+            {
+                Console.WriteLine("Le nœud de départ n'existe pas !");
+                return;
+            }
+
+            Stack<int> pile = new Stack<int>();
+            HashSet<int> visite = new HashSet<int>();
+
+            pile.Push(depart);
+            visite.Add(depart);
+
+            Console.WriteLine("Parcours en Profondeur (DFS) :");
+            while (pile.Count > 0)
+            {
+                int noeud = pile.Pop();
+                Console.Write(noeud + " ");
+
+                foreach (int voisin in listeAdjacence[noeud])
+                {
+                    if (!visite.Contains(voisin))
+                    {
+                        pile.Push(voisin);
+                        visite.Add(voisin);
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Vérifie si le graphe est connexe.
+        /// </summary>
+        /// <returns>True si le graphe est connexe, sinon False.</returns>
+        public bool EstConnexe()
+        {
+            if (noeuds.Count == 0) return false;
+
+            int premierNoeud = noeuds.Keys.First();
+            HashSet<int> visite = new HashSet<int>();
+            Queue<int> file = new Queue<int>();
+
+            file.Enqueue(premierNoeud);
+            visite.Add(premierNoeud);
+
+            while (file.Count > 0)
+            {
+                int noeud = file.Dequeue();
+                foreach (int voisin in listeAdjacence[noeud])
+                {
+                    if (!visite.Contains(voisin))
+                    {
+                        file.Enqueue(voisin);
+                        visite.Add(voisin);
+                    }
+                }
+            }
+            return visite.Count == nombreNoeuds;
+        }
+
+        /// <summary>
+        /// Vérifie si le graphe contient un cycle.
+        /// </summary>
+        /// <returns>True si un cycle est détecté, sinon False.</returns>
+        public bool ContientCycle()
+        {
+            bool[] visite = new bool[nombreNoeuds + 1];
+
+            for (int i = 1; i <= nombreNoeuds; i++)
+            {
+                if (!visite[i] && Parcours_Cycle(i, -1, visite))
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Méthode auxiliaire pour détecter un cycle via DFS.
+        /// </summary>
+        private bool Parcours_Cycle(int noeud, int parent, bool[] visite)
+        {
+            visite[noeud] = true;
+
+            foreach (int voisin in listeAdjacence[noeud])
+            {
+                if (!visite[voisin])
+                {
+                    if (Parcours_Cycle(voisin, noeud, visite)) return true;
+                }
+                else if (voisin != parent) return true;
+            }
+            return false;
         }
     }
 }
-   
-
-
-
-
-    
