a9281dc89c2b0c4b4d6cfc87a257511a09a6cf7a
Albandu78 <alban.deboichet@edu.devinci.fr>
2025-04-01 15:09:49 +0200

    Lien.cs

--- Lien.cs
+++ Lien.cs
@@ -6,35 +6,30 @@ using System.Threading.Tasks;
 
 namespace Projet_PSI
 {
-    internal class Lien
+    internal class Lien<T>
+{
+    public Noeud<T> Noeud1 { get; }
+    public Noeud<T> Noeud2 { get; }
+    //public double Distance { get; } // distance en Km
+
+
+
+    public Lien(Noeud<T> n1, Noeud<T> n2)
     {
+        Noeud1 = n1;
+        Noeud2 = n2;
+        this.Distance = CalculerDistance(n1, n2);
+    }
 
-        public Noeud Noeud1 { get; }
-        public Noeud Noeud2 { get; }
-        public double distance { get; } //distance en Km
-
-        public Lien(Noeud n1, Noeud n2)
-        {
-            Noeud1 = n1;
-            Noeud2 = n2;
-            this.distance = distance(n1, n2);
-        }
-
-        static double distance(Noeud n1, Noeud n2)
-        {
-            double n;
-            int Dlat = n2.Latitude - n1.Latitude;
-            if (Dlat <0)
-            {
-                Dlat = -Dlat;
-            }
-            int Dlong = n2.Longitude - n1.Longitude;
-            if (Dlong <0)
-            {
-                Dlong = -Dlong;
-            }
-            n = 2*6371*Math.Asin(Math.Sqrt(Math.Sin(Dlat/2)*Math.Sin(Dlat/2)+Math.Cos(n1.Latitude)*Math.Cos(n2.Latitude)*Math.Sin(Dlong/2)*Math.Sin(Dlong/2)));
-            return n;
-        }
+    static double CalculerDistance(Noeud<T> n1, Noeud<T> n2)
+    {
+        double n = 0;
+        // int Dlat = n2.latitude - n1.latitude;
+        // if (Dlat < 0) { Dlat = -Dlat; }
+        // int Dlong = n2.longitude - n1.longitude;
+        // if (Dlong < 0) { Dlong = -Dlong; }
+        // n = 2 * 6371 * Math.Asin(Math.Sqrt(Math.Sin(Dlat / 2) * Math.Sin(Dlat / 2) +
+        //     Math.Cos(n1.latitude) * Math.Cos(n2.latitude) * Math.Sin(Dlong / 2) * Math.Sin(Dlong / 2)));
+        return n;
     }
 }
