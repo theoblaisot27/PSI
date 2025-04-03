22497daa371cd5666bd2c40652c4bb5635164b3a
maxime123456789517 <mbreuil7@gmail.com>
2025-04-03 10:23:07 +0200

    Update Lien.cs

--- Lien.cs
+++ Lien.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,29 +8,36 @@ using System.Threading.Tasks;
 namespace Projet_PSI
 {
     internal class Lien<T>
-{
-    public Noeud<T> Noeud1 { get; }
-    public Noeud<T> Noeud2 { get; }
-    //public double Distance { get; } // distance en Km
-
-
-
-    public Lien(Noeud<T> n1, Noeud<T> n2)
     {
-        Noeud1 = n1;
-        Noeud2 = n2;
-        this.Distance = CalculerDistance(n1, n2);
-    }
+        public Noeud<T> Noeud1 { get; }
+        public Noeud<T> Noeud2 { get; }
+        public double Distance { get; }
 
-    static double CalculerDistance(Noeud<T> n1, Noeud<T> n2)
-    {
-        double n = 0;
-        // int Dlat = n2.latitude - n1.latitude;
-        // if (Dlat < 0) { Dlat = -Dlat; }
-        // int Dlong = n2.longitude - n1.longitude;
-        // if (Dlong < 0) { Dlong = -Dlong; }
-        // n = 2 * 6371 * Math.Asin(Math.Sqrt(Math.Sin(Dlat / 2) * Math.Sin(Dlat / 2) +
-        //     Math.Cos(n1.latitude) * Math.Cos(n2.latitude) * Math.Sin(Dlong / 2) * Math.Sin(Dlong / 2)));
-        return n;
+
+
+        public Lien(Noeud<T> n1, Noeud<T> n2)
+        {
+            Noeud1 = n1;
+            Noeud2 = n2;
+            this.Distance = CalculerDistance(n1, n2);
+        }
+
+        public static double CalculerDistance(Noeud<T> n1, Noeud<T> n2)
+        {
+            const double R = 6371;
+            double lat1 = n1.Latitude * Math.PI / 180.0;
+            double lon1 = n1.Longitude * Math.PI / 180.0;
+            double lat2 = n2.Latitude * Math.PI / 180.0;
+            double lon2 = n2.Longitude * Math.PI / 180.0;
+
+            double dlat = lat2 - lat1;
+            double dlon = lon2 - lon1;
+
+            double a = Math.Pow(Math.Sin(dlat / 2), 2) +
+                       Math.Cos(lat1) * Math.Cos(lat2) * Math.Pow(Math.Sin(dlon / 2), 2);
+            double c = 2 * Math.Asin(Math.Sqrt(a));
+
+            return R * c;
+        }
     }
 }
